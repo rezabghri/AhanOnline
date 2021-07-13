b977ab17c04149dcb6fe6b16a0a911129c4a4ace
rezabghri <reza1371bagheri@gmail.com>
2021-07-14 02:25:49 +0430

    first commit

---
+++ src/pages/axios.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import IUser from "../types/user";
+// you don't need anything else to import
+
+interface Props {
+  // TODO: add user type
+}
+
+export default function AxiosTest(props: Props) {
+  // TODO: once you get data map through data and show them name
+  return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
+}
+
+// TODO: fetch data with axios to use in app
+// RestApi: axios.get("/api/users") => [{name: "amir"}]
+// NOTE: data fetching should happen in server side
+// Resource: https://nextjs.org/docs/basic-features/data-fetching
+// Resource: https://github.com/axios/axios
